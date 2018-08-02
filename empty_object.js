8b62e47b2361799bb9a805f3fdee12d9a7300340
mohit <anand.mohit001@gmail.com>
2018-08-02 23:57:03 +0530

    Objs with props

--- empty_object.js
+++ empty_object.js
@@ -1,2 +1,11 @@
-var x = {};
-console.log(x);
+// empty obj 
+
+var my_obj = {};
+console.log(my_obj);
+
+my_obj.foo = "hello" ;
+my_obj.bar = "hi" ; 
+
+// js is dynamicallly typed remember
+my_obj.foo = 12 ;
+
