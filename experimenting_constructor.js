63e2b65e7d91fb711a64563401178822c043afa3
mohit <anand.mohit001@gmail.com>
2018-08-03 01:28:57 +0530

    Experimenting with constructor functions

--- experimenting_constructor.js
+++ experimenting_constructor.js
@@ -13,3 +13,23 @@ console.log(x) ;
 var  y = regular("hello", "bello", "female");
 console.log(y);
 
+console.log('------------');
+
+// for consturctor_functions it is a convention
+// that we need to start the name with capital
+
+function Consturctor_Employee(first_name, last_name, gender) {
+	
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.gender = gender; 
+}
+
+// p is undefined, since the function does not return anything, and the new keyword is not used
+// the function Consturctor_Employee takes the 'this' of the global context and 
+// changes it, 
+var p = Consturctor_Employee("abcde", "fghij", "female");
+console.log(p) ;
+console.log(this.first_name, this.last_name, this.gender);
+var z = new Consturctor_Employee("hello", "bello", "female");
+console.log(z) ;
