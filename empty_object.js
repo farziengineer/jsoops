b9acb4bf66c7515aa0bfac1a1b53689edd545d4b
mohit <anand.mohit001@gmail.com>
2018-08-03 00:00:29 +0530

    Concise way to set obj values

--- empty_object.js
+++ empty_object.js
@@ -1,7 +1,6 @@
 // empty obj 
 
 var my_obj = {};
-console.log(my_obj);
 
 my_obj.foo = "hello" ;
 my_obj.bar = "hi" ; 
@@ -9,3 +8,12 @@ my_obj.bar = "hi" ;
 // js is dynamicallly typed remember
 my_obj.foo = 12 ;
 
+
+my_obj = {
+	"foo" : "hello" ,
+	"shoo" : "doo" ,
+	"hi" : 123,
+
+};
+
+console.log(my_obj);
