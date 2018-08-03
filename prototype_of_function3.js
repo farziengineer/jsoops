4bed17a7b049cdea49b8129ff66a2d8b2f6f2afb
mohit <anand.mohit001@gmail.com>
2018-08-03 13:06:54 +0530

    prototype chaining example

--- prototype_of_function3.js
+++ prototype_of_function3.js
@@ -12,9 +12,9 @@ console.log(obj1);
 // when we write obj1.name the js engine first searches the prop
 // `name` in the obj1 itself, it finds it and logs it.
 console.log(obj1.name);
+
 // when we write obj1.age the js engine finds searches the prop
 // `age` in the obj1, then does not find it and goes to lookup
 // where __proto__ is pointing, and finds it, and logs it
+
 console.log(obj1.age);
-
-
