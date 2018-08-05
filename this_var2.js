4d388b74601b65b29b30378caebe99dc5674c9f8
mohit <anand.mohit001@gmail.com>
2018-08-05 16:32:16 +0530

    Global namespace pollution

---
+++ this_var2.js
@@ -0,0 +1,22 @@
+var Con = function() {
+	this.poor = 23;
+
+	this.func = function() {
+		console.log(this.poor);
+	}
+}
+
+// the above is a constructor function
+// and should be used for the same, 
+// constructing objects. We should be careful 
+// as to not call it as it might pollute 
+// global namespace
+// example, 
+// this will attach a poor property to
+// the global this, and 
+// a func function to global func 
+Con();
+
+console.log(poor);
+console.log(func());
+
