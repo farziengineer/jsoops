af4451b7278e5ab313cd4b7ee91dacc12d3d4b59
mohit <anand.mohit001@gmail.com>
2018-08-07 22:54:43 +0530

    Added currying practical use

---
+++ functional_pr/currying_create_functions.js
@@ -0,0 +1,25 @@
+// https://stackoverflow.com/questions/113780/javascript-curry-what-are-the-practical-applications
+// currying is used to generate functions
+// In the following example, I will generate 
+// functions which convert from one unit of measurement 
+// to another
+
+function gener(factor){
+
+	function func(inp){
+
+		return factor * inp;
+	};
+
+	return func;
+};
+
+var metre_to_centi = gener(100);
+console.log(metre_to_centi(12));
+
+var kg_to_gram = genr(1000);
+console.log(kg_to_gram(98));
+
+// we can create separate functions for these
+// kg_to_gram and metre_to_centi functions, individually
+// but this code is modular
