4b61984e8dd53f199aa86e615bc8fa5a4fd22d6f
mohit <anand.mohit001@gmail.com>
2018-08-07 16:30:49 +0530

    Added first order functions example

---
+++ functional_pr/higher_order_functions.js
@@ -0,0 +1,30 @@
+// implementing filters 
+
+
+function filterx(arr, the_filter){
+
+	var filtered = [];
+	for(var i = 0; i < arr.length; i +=1){
+		if(the_filter(arr[i]) == true){
+			filtered.push(arr[i]);
+		}
+	}
+
+	return filtered;
+}
+
+var arr = [1, 2, 3, 4, 5, 7, 12, 123, 54];
+
+var res1 = filterx(arr, function(num){
+	return (num%2 === 0);
+});
+
+console.log(res1);
+
+console.log('-------');
+
+var res2 = filterx(arr, function(num){
+	return (num > 5);
+});
+console.log(res2);
+
